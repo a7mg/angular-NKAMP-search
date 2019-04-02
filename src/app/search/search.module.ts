a1cdf7ccc0a4beaf6693d30556755833c3d8b842
Ahmed ElBaz <aelbaz@cairo.aas.com.sa>
2019-04-02 07:35:51 +0200

    Edit stracture of Components

--- src/app/search/search.module.ts
+++ src/app/search/search.module.ts
@@ -2,20 +2,20 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SearchComponent } from '../search/search.component';
 import { RouterModule } from '@angular/router';
-import { FiltersComponent } from './filters/filters.component';
 import { ResultsComponent } from './results/results.component';
 import { CriteriaComponent } from './criteria/criteria.component';
 import { ProgressSpinnerModule } from 'primeng/progressspinner';
 import { FacetsComponent } from './facets/facets.component';
-import { GridComponent } from './results/grid/grid.component';
-import { ListComponent } from './results/list/list.component';
+import { GridComponent } from '../common/grid/grid.component';
+import { ListComponent } from '../common/list/list.component';
+import { FiltersComponent } from './results/filters/filters.component';
 
 const HeaderRoutes = [
   { path: '', component: SearchComponent }
 ];
 
 @NgModule({
-  declarations: [SearchComponent, FiltersComponent, ResultsComponent, CriteriaComponent, FacetsComponent, GridComponent, ListComponent],
+  declarations: [SearchComponent, ResultsComponent, CriteriaComponent, FacetsComponent, GridComponent, ListComponent, FiltersComponent],
   imports: [
     RouterModule.forChild(HeaderRoutes),
     CommonModule,
