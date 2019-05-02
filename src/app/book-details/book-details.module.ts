68c55d81a8ee4b2933a4ec9feafc109fdd964067
Hager Essa <Hager.Essa@nassej.com>
2019-05-02 09:04:33 +0200

    add book details

--- src/app/book-details/book-details.module.ts
+++ src/app/book-details/book-details.module.ts
@@ -1,10 +1,33 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
+import {RatingModule} from 'primeng/rating';
+import { BookDetailsService } from './sevrices/book-details.service';
+import { BookDetailsComponent } from './book-details.component';
+import { DetailsComponent } from './details/details.component';
+import { ReviewRatingComponent } from './review-rating/review-rating.component';
+import { AddReviewComponent } from './add-review/add-review.component';
+import { RouterModule } from '@angular/router';
 
+
+const HeaderRoutes = [
+  { path: '', component:  BookDetailsComponent }
+];
 @NgModule({
-  declarations: [],
+  declarations: [
+    BookDetailsComponent,
+    DetailsComponent, 
+    ReviewRatingComponent, 
+    AddReviewComponent
+  ],
+  providers:[
+    BookDetailsService
+  ],
   imports: [
-    CommonModule
+    RouterModule.forChild(HeaderRoutes),
+    CommonModule,
+    FormsModule,
+    RatingModule
   ]
 })
 export class BookDetailsModule { }
