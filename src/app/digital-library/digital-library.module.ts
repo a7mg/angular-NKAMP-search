ec59947400d3e2ed2f50057e4a39af46ece2cf73
Hager Essa <Hager.Essa@nassej.com>
2019-05-07 08:35:42 +0200

    digital

--- src/app/digital-library/digital-library.module.ts
+++ src/app/digital-library/digital-library.module.ts
@@ -1,10 +1,25 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { GridComponent } from '../Common/grid/grid.component';
+
+import { SwiperModule, SwiperConfigInterface,
+  SWIPER_CONFIG } from 'ngx-swiper-wrapper';
+const DEFAULT_SWIPER_CONFIG: SwiperConfigInterface = {
+  observer: true,
+  direction: 'horizontal',
+  threshold: 50,
+  spaceBetween: 5,
+  slidesPerView: 1,
+  centeredSlides: true
+};
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    GridComponent
+  ],
   imports: [
-    CommonModule
+    CommonModule,
+    SwiperModule
   ]
 })
 export class DigitalLibraryModule { }
