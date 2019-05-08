462487d3994fb996703e34b1be8a1407d3bf43fe
Ahmed ElBaz <aelbaz@cairo.aas.com.sa>
2019-05-08 14:30:54 +0200

    update Authorization

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,8 +3,6 @@ import { NgModule } from '@angular/core';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { DigitalLibraryComponent } from './digital-library/digital-library.component';
-import { BookDetailsComponent } from './book-details/book-details.component';
 import { OrderByPipe } from './Naseej-shared/order-by.Pipe';
 import { NkampInterceptor } from './interceptor';
 
