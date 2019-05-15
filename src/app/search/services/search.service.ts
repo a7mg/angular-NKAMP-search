148777bb9c8744564dc4cc7d3e6062e85b8d8018
Hager Essa <Hager.Essa@nassej.com>
2019-05-15 13:30:03 +0200

    add new function in service search file

--- src/app/search/services/search.service.ts
+++ src/app/search/services/search.service.ts
@@ -62,4 +62,24 @@ export class SearchService {
     );
   }
 
+  addQuery(serachCriteria): Observable<any> {
+    return this.http.post<any>(this.Url + 'AddQuery', serachCriteria).pipe(
+      map((data: any) => {
+        return data;
+      }), catchError((error: Error) => {
+        const errParams: any[] = [];
+        errParams.push(`API_URL = ${this.Url}`);
+        errParams.push(`UILanguage = ${this.globals.UILanguage}`);
+        this.errorLogging.error(
+          'MakeNewSearch',
+          `${error.name} --> ${error.message} --> ${error.stack}` ||
+          `${error.name} --> ${error.message}`,
+          errParams
+        );
+        return of([] as any[]);
+      })
+    );
+  }
+  
+  
 }
