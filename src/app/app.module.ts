345020db747a08a64eaf02ae37d84f3fe31f5d18
CAIRO\HEssa <hagersaeedmahmoud@gmail.com>
2019-09-03 12:01:46 +0200

    font error

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,25 @@ import { OrderByPipe } from './NKAMP-Search-shared/order-by.Pipe';
 import { NkampInterceptor } from './interceptor';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NKAMPSearchSharedModule } from './NKAMP-Search-shared/NKAMP-Search-shared.module';
-
+import { ShareButtonsModule, ShareButtonsOptions } from 'ngx-sharebuttons';
+const options: ShareButtonsOptions = {
+  include: ['facebook', 'twitter', 'google'],
+  exclude: ['tumblr', 'stumble', 'vk'],
+  theme: 'modern-light',
+  gaTracking: true,
+  twitterAccount: 'twitterUsername'
+};
+const buttonsConfig = {
+  facebook: {
+    icon: 'fa fa-facebook-official',
+    text: 'Share on Facebook'
+  },
+  twitter: {
+    icon: 'fa fa-twitter-square',
+    text: 'Tweet'
+  },
+  // and so on...
+};
 @NgModule({
   declarations: [
     AppComponent,
@@ -16,7 +34,9 @@ import { NKAMPSearchSharedModule } from './NKAMP-Search-shared/NKAMP-Search-shar
     BrowserAnimationsModule,
     AppRoutingModule,
     HttpClientModule,
-    NKAMPSearchSharedModule.forRoot()
+    NKAMPSearchSharedModule.forRoot(),
+    ShareButtonsModule.forRoot(options, buttonsConfig)
+    
   ],
   providers: [
     {
