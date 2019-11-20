fcbdcfdbbd932313be023f502e05744461333360
oMnia Elsayed <Omnia.Morsi@naseej.com>
2019-11-20 10:46:21 +0200

    Favorite Page - completed

--- src/app/favorite/services/favorite.service.ts
+++ src/app/favorite/services/favorite.service.ts
@@ -24,7 +24,9 @@ export class FavoriteService {
   }
 
   getFavoriteList(requestBody): Observable<any> {
-    requestBody.wantedPage = this.nextPageCriteria.wantedPage;
+
+    // requestBody.wantedPage = this.nextPageCriteria.wantedPage;
+    // return this.http.post<any>(this.Url + 'GetFavoritesList', requestBody, this.bookDetailsService.httpOptions);
     return this.http.post<any>(this.Url + 'GetFavoritesList', requestBody, this.bookDetailsService.httpOptions).pipe(
       map((data: any) => {
         return data;
